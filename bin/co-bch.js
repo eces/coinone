404ebcd019a3cecd2ae2769d3f32d4aa7d2de819
Jinhyuk Lee <eces@mstock.org>
2018-03-18 17:06:24 +0900

    Notice unsupported

---
+++ bin/co-bch.js
@@ -0,0 +1,5 @@
+const chalk = require('chalk')
+
+console.log(chalk.white.bgBlue(' 미지원 '), chalk.blue('지원예정입니다.'))
+
+return
--- bin/index.js
+++ bin/index.js
@@ -13,9 +13,26 @@ program
 program
   .command('whoami', '로그인된 계정을 확인합니다.')
 
-  program
+program
   .command('xrp', 'XRP 리플코인 호가를 조회합니다.')
   .option('-n --lines', '표시갯수')
   .option('-s --scale', '호가단위')
+
+program
+  .command('btc', 'btc')
+program
+  .command('bch', 'bch')
+program
+  .command('eth', 'eth')
+program
+  .command('etc', 'etc')
+program
+  .command('qtum', 'qtum')
+program
+  .command('iota', 'iota')
+program
+  .command('ltc', 'ltc')
+program
+  .command('btg', 'btg')
 
 program.parse(process.argv);
